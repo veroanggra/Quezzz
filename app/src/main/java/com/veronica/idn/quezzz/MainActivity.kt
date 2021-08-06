5f48ff5707efaafe468988f244f58b12f8494a62
veroanggra <veronicaputri497@gmail.com>
2021-08-06 13:54:31 +0700

    Initial commit

---
+++ app/src/main/java/com/veronica/idn/quezzz/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.veronica.idn.quezzz
+
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.ui.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         drawerLayout= findViewById(R.id.drawer_layout)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+}
---
+++ app/src/main/java/com/veronica/idn/quezzz/model/Quiz.kt
@@ -0,0 +1,6 @@
+package com.veronica.idn.quezzz.model
+
+data class Quiz (
+    val text: String,
+    val answers: List<String>
+)
